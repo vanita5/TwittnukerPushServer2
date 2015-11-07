6b2b93a9dfe3f447319315dc931e10faf5d1ebf8
vanita5 <mail@vanita5.de>
2015-11-08 00:58:38 +0100

    implement register/removing of reg ids, Twitter user stream, sending messages via gcm

---
+++ handler.js
@@ -0,0 +1,146 @@
+var gcm = require('node-gcm'),
+    config = require('./config'),
+    lowdb = require('lowdb');
+
+module.exports = {
+    streamHandler: function(stream, userId) {
+
+        var db = lowdb('db.json');
+        var sender = new gcm.Sender(config.gcm.api_key);
+
+        stream.on('tweet', function(status) {
+            if (isMyRetweet(status, userId)) {
+                console.info('Retweet from ' + status.user.screen_name);
+                notify(
+                    userId,
+                    status.user.screen_name,
+                    'type_retweet',
+                    status.retweeted_status.text,
+                    status.user.profile_image_url
+                );
+            }
+            else if (isMention(status, userId)) {
+                console.info('Mention from ' + status.user.screen_name);
+                notify(
+                    userId,
+                    status.user.screen_name,
+                    'type_mention',
+                    status.text,
+                    status.user.profile_image_url
+                );
+            }
+        });
+
+        stream.on('direct_message', function (dm) {
+            console.info('DM from ' + dm.direct_message.sender.screen_name);
+            notify(
+                userId,
+                dm.direct_message.sender.screen_name,
+                'type_direct_message',
+                null, //dm.direct_message.text,
+                dm.direct_message.sender.profile_image_url
+            );
+        });
+
+        stream.on('follow', function (event) {
+            if (isMyEvent(event, userId)) {
+                console.log('Follow from ' + event.source.screen_name);
+                notify(
+                    userId,
+                    event.source.screen_name,
+                    'type_new_follower',
+                    null,
+                    event.source.profile_image_url
+                );
+            }
+        });
+
+        stream.on('favorite', function (event) {
+            if (isMyEvent(event, userId)) {
+                console.log('Like from ' + event.source.screen_name);
+                notify(
+                    userId,
+                    event.source.screen_name,
+                    'type_favorite',
+                    event.target_object.text,
+                    event.source.profile_image_url
+                );
+            }
+        });
+
+        stream.on('connect', function (request) {
+            console.info("Connecting to connect to user stream...");
+        });
+
+        stream.on('connected', function (response) {
+            console.info("User stream connected.")
+        });
+
+        stream.on('reconnect', function (request, response, connectInterval) {
+            console.info("Reconnecting to user stream...")
+        });
+
+        stream.on('disconnect', function(message) {
+            console.error("Stream disconnected with message: " + message);
+            notifyDisconnect();
+        });
+
+
+        var isMyRetweet = function(status, userId) {
+            return status.retweeted_status
+                && status.retweeted_status.user.id_str == userId;
+        };
+
+        var isMention = function(status, userId) {
+            var result = false;
+            status.entities.user_mentions.forEach(function(mention) {
+                if (mention.id_str == userId) result = true;
+            });
+            return result;
+        };
+
+        var isMyEvent = function(event, userId) {
+            return event.source.id_str != userId;
+        };
+
+        var notify = function(userId, fromuser, type, msg, image_url) {
+            var message = new gcm.Message({
+                priority: "high",
+                delayWhileIdle: false,
+                restrictedPackageName: "de.vanita5.twittnuker",
+                data: {
+                    account: userId,
+                    fromuser: fromuser,
+                    type: type,
+                    msg: msg,
+                    image: image_url
+                }
+            });
+
+            var regIds = db('tokens').pluck('token');
+            sender.send(message, { registrationIds: regIds }, function(err, result) {
+                if (err) console.error(err);
+                else     console.log(result);
+            });
+        };
+
+        var notifyDisconnect = function() {
+            var message = new gcm.Message({
+                restrictedPackageName: "de.vanita5.twittnuker",
+                notification: {
+                    title: "Twittnuker",
+                    icon: "ic_action_twittnuker",
+                    body: "Push Backend Server halted."
+                }
+            });
+
+            var regIds = db('tokens').pluck('token');
+            sender.send(message, { registrationIds: regIds }, function(err, result) {
+                if (err) console.error(err);
+                else     console.log(result);
+
+
+            });
+        };
+    }
+};
--- index.js
+++ index.js
@@ -1,44 +1,119 @@
 var express = require('express'),
-    gcm = require('node-gcm'),
-    config = require('./config');
+    twit = require('twit'),
+    lowdb = require('lowdb'),
+    streamhandler = require('./handler');
+
+try {
+    var config = require('./config');
+    if (!config.gcm || !config.twitter) throw "config file corrupted";
+} catch (error) {
+    console.error(error);
+    console.error(
+        "Error loading config file.\n" +
+        "\n" +
+        "Please copy config_example.js to config.js and edit accordingly!"
+    );
+    process.exit(1);
+}
 
 var app = express();
 
-var sender = new gcm.Sender(config.gcm.api_key);
+var db = lowdb('db.json');
 
+var twitter = {
+    instances: []
+};
 
+var initTwitterInstances = function() {
+    if (config.twitter.length == 0) {
+        console.error("No Twitter configurations. Please edit config.js accordingly!");
+    }
+    else if (db('tokens').size() == 0) {
+        console.info("No device registered. Please setup Twittnuker to receive Push Notifications from this server!")
+    }
+
+    config.twitter.forEach(function(profile) {
+        var T = new twit({
+            consumer_key:         profile.consumer_key
+            , consumer_secret:      profile.consumer_secret
+            , access_token:         profile.access_token
+            , access_token_secret:  profile.access_token_secret
+        });
+
+        var stream = null;
+        var userId = null;
+        T.get('account/verify_credentials', { skip_status: true }, function (err, data, response) {
+            stream = T.stream('user');
+            stream.on('connect', function() {streamhandler.streamHandler(stream, data.id_str)});
+            userId = data.id_str;
+        });
+
+        var instance = {
+            twit: T,
+            stream: stream,
+            userId: userId
+        };
+
+        twitter.instances.push(instance);
+    });
+};
+
+var updateTwitterInstances = function() {
+    twitter.instances.forEach(function(instance) {
+        instance.stream.stop();
+    });
+    twitter.instances = [];
+    initTwitterInstances();
+};
+
+/**
+ *
+ */
 app.get('/', function(req, res) {
     res.send('This is a Twittnuker Push Server instance. Set this address to the respective setting in Twittnuker to receive push notifications.');
 });
 
+/**
+ * POST register
+ * Adds a token to the database if the given Twitter user id is configured
+ */
+app.post('/register', function(req, res) {
+    var token = res.params.token;
+    var twitterUserId = res.params.userId;
+
+    var isConfigured = false;
+    twitter.instances.forEach(function(instance) {
+        if (instance.userId == twitterUserId) isConfigured = true;
+    });
+    if (isConfigured && !db('tokens').find({ token: token })) {
+        db('tokens').push({token: token});
+    }
+    updateTwitterInstances();
+
+    res.set('Content-Type', 'application/json');
+    res.send('{ "status": "Ok" }');
+});
+
+/**
+ * POST remove
+ * Remove token from database
+ */
+app.post('/remove', function(req, res) {
+    var token = res.params.token;
+    db('tokens').remove({ token: token });
+    updateTwitterInstances();
+
+    res.set('Content-Type', 'application/json');
+    res.send('{ "status": "Ok" }');
+});
+
+/**
+ * Init
+ */
 var server = app.listen(7331, function() {
     var host = server.address().address;
     var port = server.address().port;
 
     console.info('Twittnuker Push running at http://%s:%s', host, port);
 });
-
-
-// TEST
-var test = new gcm.Message({
-    priority: 'normal',
-    contentAvailable: true,
-    delayWhileIdle: true,
-    restrictedPackageName: "de.vanita5.twittnuker",
-    data: {
-        message: 'message1',
-        key2: 'message2'
-    },
-    notification: {
-        title: "Twittnuker",
-        icon: "ic_action_retweet",
-        body: "@_vanita5 liked your tweet."
-    }
-});
-
-var regToken = ['REG ID'];
-
-sender.send(test, { registrationIds: regToken }, function(err, result) {
-    if (err) console.error(err);
-    else     console.log(result);
-});
+updateTwitterInstances();
