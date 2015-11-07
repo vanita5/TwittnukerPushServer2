aa4bdaef5a1260ed7833d7e54d793fe07a645b1e
vanita5 <mail@vanita5.de>
2015-11-07 17:20:13 +0100

    add config example

---
+++ config_example.js
@@ -0,0 +1,13 @@
+module.exports = {
+    gcm: {
+        api_key: "AIzaSyA6ZYhidGZ_5kiIvMmQ0O1z9wVeCRzJOM4"
+    },
+    twitter: [
+        {
+            consumer_key: "YOUR_CONSUMER_KEY",
+            consumer_secret: "YOUR_CONSUMER_SECRET",
+            access_token: "YOUR_ACCESS_TOKEN",
+            access_token_secret: "YOUR_ACCESS_TOKEN_SECRET"
+        }
+    ]
+};
